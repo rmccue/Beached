ff69c22f1ede9be7b0d8e86d9622998eedbbee29
Ryan McCue <ryanmccue@cubegames.net>
2009-11-02 21:31:36 +1000

    Use widget sidebar

--- header.php
+++ header.php
@@ -11,24 +11,7 @@
 			<h1><?php bloginfo('name') ?></h1>
 			<p><?php bloginfo('description') ?></p>
 			<ul>
-				<li class="widget">
-					<h2>Widget</h2>
-					<p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</p>
-				</li>
-
-				<li class="widget">
-					<h2>Widget</h2>
-					<p>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</p>
-				</li>
-				<li class="widget">
-					<h2>Widget</h2>
-					<p>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</p>
-
-				</li>
-				<li class="widget">
-					<h2>Widget</h2>
-					<p>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</p>
-				</li>
+				<?php dynamic_sidebar() ?>
 			</ul>
 		</div>
 		<div id="content">
